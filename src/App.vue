226457af15cd6c083dc82a74f1142a150ffc2a45
zeddo123 <m.drissi@protonmail.com>
2021-10-22 22:03:54 +0200

    Add openpi icon on the page
    update readme.md file
    
    update .gitignore
    
    remove vscode directory

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Title from './components/Title.vue'
+import MissionStatement from './components/MissionStatement.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <img alt="openpi-log" src="./assets/logo.png" />
+  <Title msg="Welcome to Openpi" />
+  <MissionStatement/>
 </template>
 
 <style>
---
+++ src/components/MissionStatement.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+
+<template>
+  <h1>Mission Statement</h1>
+
+  <p>
+    We believe that...
+  </p>
+</template>
+
+<style scoped>
+</style>
